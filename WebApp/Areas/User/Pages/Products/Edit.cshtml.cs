dd8fdb812899f74a708f0b9384b42bab8126dfab
Hakim Ghods <ghods.hakim@hotmail.com>
2020-11-23 18:24:25 +0330

    Fix

--- WebApp/Areas/User/Pages/Products/Edit.cshtml.cs
+++ WebApp/Areas/User/Pages/Products/Edit.cshtml.cs
@@ -4,8 +4,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.SignalR;
 using WebApp.Common.Extentions;
 using WebApp.Data;
+using WebApp.Hubs;
 using WebApp.Models;
 
 namespace WebApp.Areas.User.Pages.Products
@@ -13,12 +15,13 @@ namespace WebApp.Areas.User.Pages.Products
     public class EditModel : PageModel
     {
         private readonly ApplicationDbContext _db;
+        private readonly IHubContext<ProductHub> _hub;
 
-        public EditModel(ApplicationDbContext db)
+        public EditModel(ApplicationDbContext db, IHubContext<ProductHub> hub)
         {
             _db = db;
+            _hub = hub;
         }
-
         public IActionResult OnGet(int id)
         {
             Product = _db.Products.Find(id);
@@ -28,12 +31,14 @@ namespace WebApp.Areas.User.Pages.Products
         [BindProperty]
         public Product Product { get; set; }
 
-        public IActionResult OnPost()
+        public async Task<IActionResult> OnPost()
         {
             var product = _db.Products.Find(Product.Id);
 
             product.IsActive = Product.IsActive;
             product.Price = Product.Price;
+
+            await _hub.Clients.All.SendAsync("UpdateProduct", product);
 
             _db.SaveChanges();
 
